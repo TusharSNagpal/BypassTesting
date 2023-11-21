0d142379f0254c091beea21c43cbf89035f9c3d0
TusharSNagpal <nagpal8795@gmail.com>
2023-11-21 22:21:13 +0530

    spring-boot-initializer

---
+++ src/main/java/com/example/bypasstesting/BypassTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bypasstesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BypassTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BypassTestingApplication.class, args);
+    }
+
+}
