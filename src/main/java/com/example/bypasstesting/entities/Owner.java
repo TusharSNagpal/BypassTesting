d2f79e86a8d6684ae7bd639973e808518ef50f1f
TusharSNagpal <nagpal8795@gmail.com>
2023-11-21 22:46:05 +0530

    entities

---
+++ src/main/java/com/example/bypasstesting/entities/Owner.java
@@ -0,0 +1,12 @@
+package com.example.bypasstesting.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "owner")
+public class Owner {
+}
---
+++ src/main/java/com/example/bypasstesting/repositories/OwnerRepo.java
@@ -0,0 +1,6 @@
+package com.example.bypasstesting.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepo extends JpaRepository<Owner, Integer>{
+
+}
